f31bb9bcb9028236faaaa7f1b299820c882647d0
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2022-12-07 08:50:43 -0300

    Placing pieces in board

---
+++ src/chess/pieces/Rook.java
@@ -0,0 +1,18 @@
+package chess.pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Rook extends ChessPiece {
+
+	public Rook(Board board, Color color) {
+		super(board, color); // passando para a superclass
+	}
+	
+	@Override
+	public String toString() { // repassando o que vai ficar no tabuleiro
+		return "R";
+	}
+	
+}
