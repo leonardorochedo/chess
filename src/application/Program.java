59c38121ae56252ecaed59e591cd17d086a7e883
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2023-01-03 13:32:31 -0300

    Fix: Program and UI class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,3 +1,5 @@
+package application;
+
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -9,12 +11,13 @@ import chess.ChessPiece;
 import chess.ChessPosition;
 
 public class Program {
-	public static void main(String[] args) {
 
+	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
-
+		
 		while (!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
@@ -22,20 +25,20 @@ public class Program {
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
-
+				
 				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
 				UI.clearScreen();
 				UI.printBoard(chessMatch.getPieces(), possibleMoves);
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
-
+				
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-
+				
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
-
+				
 				if (chessMatch.getPromoted() != null) {
 					System.out.print("Enter piece for promotion (B/N/R/Q): ");
 					String type = sc.nextLine().toUpperCase();
@@ -45,10 +48,12 @@ public class Program {
 					}
 					chessMatch.replacePromotedPiece(type);
 				}
-			} catch (ChessException e) {
+			}
+			catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
-			} catch (InputMismatchException e) {
+			}
+			catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			}
